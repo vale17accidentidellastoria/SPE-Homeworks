8b7b4a69c9c38535eba7a42ec9414eb468709198
vale17accidentidellastoria <pingupucca@gmail.com>
2020-03-25 16:54:32 +0100

    improved csv import for dataset of exercise 5

--- Homework1/spe_h1_code.py
+++ Homework1/spe_h1_code.py
@@ -7,11 +7,13 @@ def loadCSV(csv_file):
     #print(dataset.shape)
     data_points = []
     column_dim = dataset.shape[1] #save the number of columns
+    row_dim = dataset.shape[0] #save the number of columns
     if (column_dim == 1): #check if we have data organized in a single column
         data_points = dataset[0].values.tolist() #convert the values of the column into a list of values 
     elif (column_dim > 1): #check if data is expressed as a matrix of values (multiple columns)
         data_points = dataset.values.tolist()
-    #TODO: check if we have only a single row of values
+        if(row_dim == 1): #check if data is expressed as a single row
+            data_points = data_points[0]
     return data_points
 
 def computeMedian(x):
