7f89a71b91f180979b754ebab2a3b2ad6254766c
carbogninalberto <alberto.carbognin@studenti.unitn.it>
2020-03-29 15:41:44 +0200

    ex4

--- Homework1/spe_h1_code.py
+++ Homework1/spe_h1_code.py
@@ -3,11 +3,12 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 import random
+import scipy.stats as st
 
 #---------------------------------------------------
 # Settings
 #---------------------------------------------------
-METRIC = {'median': 'computeMedian', 'mean':'computeMean'}
+METRIC = {'median': 'computeMedian', 'mean':'computeMean', 'variance':'computeVar', 'log_mean':'computeLogMean'}
 #---------------------------------------------------
 def loadCSV(csv_file):
     dataset = pd.read_csv(csv_file, header=None) #here we are working with Pandas DataFrame
@@ -43,6 +44,16 @@ def computeMean(x):
     mean = sum / n
     return mean
 
+def computeLogMean(x):
+    prod = 1
+    n = len(x)
+    for i in range(0,n):
+        prod *= x[i]
+    mean = prod**(1/n)
+    #if not isinstance(mean, complex):
+        #print('mean:', mean, 'prod:', prod)
+    return mean
+
 def computeStdDev(x, mean):
     sum_squares = 0
     n = len(x)
@@ -50,6 +61,32 @@ def computeStdDev(x, mean):
         sum_squares += pow((x[i] - mean), 2)
     std_dev = math.sqrt(sum_squares / (n -1)) #divide by n or (n-1)??
     return std_dev
+
+def getCIMedian(data, ci_value):
+    eta = st.t.ppf((1 + ci_value) / 2, len(data) - 1)
+    n = len(data)
+    j = int(round(0.5*n - eta*math.sqrt(0.5*n*(1-0.5))))
+    k = int(round(0.5*n + eta*math.sqrt(0.5*n*(1-0.5)))) + 1
+    return data[j-1], data[k-1]
+
+def getCIMean(data, ci_value):
+    eta = st.t.ppf((1 + ci_value) / 2, len(data) - 1)
+    print(eta)
+    #np_mean = np.mean(data) #numpy mean
+    mean = computeMean(data)
+    
+    #np_sem = st.sem(data) #scipy stats standard error mean
+    std_error_mean = computeStdDev(data, mean) / math.sqrt(len(data))
+    conf_interval = eta * std_error_mean
+
+    start_int = mean - conf_interval
+    end_int = mean + conf_interval
+
+    return start_int, end_int
+
+def computeVar(x, mean):
+    std_dev = computeStdDev(x, mean)
+    return std_dev**2
 
 def computeCov(std_dev, mean):
     coV = std_dev / mean
@@ -105,13 +142,18 @@ def printLorenzCurveGap(p, l):
 def bootstrapAlgorithm(dataset, accuracy=25, ci_level=0.95, metric='mean'):
     ds_length = len(dataset)
     samples_metric = []
+
     samples_metric.append(globals()[METRIC[metric]](dataset))
     R = math.ceil(2 * (accuracy / (1-ci_level))) - 1
+
+    print('bs mean', samples_metric)
+
     for r in range(R):
         tmp_dataset = []
         for i in range(ds_length):
             tmp_dataset.append(dataset[random.randrange(0, ds_length, 1)])
-        samples_metric.append(globals()[METRIC[metric]](tmp_dataset))
+        samples_metric.append(globals()[METRIC[metric]](tmp_dataset)) # load the desired metric function
+
     samples_metric.sort()
     print('sample_metric_len:', len(samples_metric), 'range len:', len(samples_metric[accuracy:(R+1-accuracy)]))
     return samples_metric[accuracy:(R+1-accuracy)]
@@ -120,6 +162,20 @@ def printBootsrapMetric(metric):
     plt.plot(metric, np.zeros_like(metric), '.', linewidth=2.0)
     plt.show()
 
+def exercise2(dataset):
+    # TODO: compute first row CI 95%
+    ci = [3, 10]
+
+    means = []
+    for row in dataset[1:]:
+        means.append(computeMean(row))
+    
+    counter_mean = 0
+    for mean in means:
+        if ci[0] <= mean <= ci[1]:
+            counter_mean += 1
+    
+    print(counter_mean, "out of 6000 means are inside the CI", ci)
 
 #---------------------------------------------------
 
@@ -130,16 +186,15 @@ median1 = computeMedian(data1)
 print("\t The Median is", median1)
 mean1 = computeMean(data1)
 print("\t The Mean is", mean1)
-printBootsrapMetric(bootstrapAlgorithm(dataset=data1, metric='median'))
+
 
 print("\n####################")
 #---------------------------------------------------
 
 print("\nExercise 2")
 data2 = loadCSV("data_hw1/data_ex2.csv")
-#print(data2)
 
-#solution for Exercise 2
+exercise2(data2)
 
 print("\n####################")
 #---------------------------------------------------
@@ -169,6 +224,30 @@ print("\n####################")
 print("\nExercise 4")
 
 data4 = loadCSV("data_hw1/data_ex4.csv")
+bs_95 = bootstrapAlgorithm(dataset=data4)
+bs_99 = bootstrapAlgorithm(dataset=data4, ci_level=0.99)
+
+print('95% CI with bootstrap is [{}, {}]'.format(bs_95[0], bs_95[len(bs_95)-1]))
+print('99% CI with bootstrap is [{}, {}]'.format(bs_99[0], bs_99[len(bs_99)-1]))
+
+start_ci_mean4_95, end_ci_mean4_95 = getCIMean(data4, 0.95)
+start_ci_mean4_99, end_ci_mean4_99 = getCIMean(data4, 0.99)
+
+print('95% CI with asymptotic formulas is [{}, {}]'.format(start_ci_mean4_95, end_ci_mean4_95))
+print('99% CI with asymptotic formulas is [{}, {}]'.format(start_ci_mean4_99, end_ci_mean4_99))
+
+log_transformed_data4 = [math.log(x+1) for x in data4]
+print('log transformation', log_transformed_data4[:5])
+for e in log_transformed_data4:
+    if e < 0:
+        print(e)
+log_mean = computeLogMean(log_transformed_data4)
+print('log_mean =', log_mean)
+
+log_bs_95 = bootstrapAlgorithm(dataset=log_transformed_data4, metric='log_mean')
+print('Log mean with 95% CI with bootstrap is [{}, {}]'.format(log_bs_95[0], log_bs_95[len(log_bs_95)-1]))
+
+#printBootsrapMetric()
 
 #solution for Exercise 4
 
