d1597ae619fabfb92945ae9878ade3d75c08baa4
vale17accidentidellastoria <pingupucca@gmail.com>
2020-04-19 09:05:23 +0200

    uploaded files for Homework 4

---
+++ Homework 4/spe_h4_code.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import random
+import scipy.stats as st
+import statsmodels.api as sm
+from scipy.spatial import distance
+
+random.seed()
+
+
+# --------------------------------------------
+
+# Exercise 1
+
+
+# --------------------------------------------
+
+# Exercise 2
+
+
+# --------------------------------------------
+
+# Exercise 3
+
+
+# --------------------------------------------
